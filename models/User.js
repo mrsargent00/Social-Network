52f1f139b0bd18bad7255579e42fbfdc585419d4
Fluffy <mrsargent00@gmail.com>
2024-02-01 18:05:46 -0500

    allow user to be made

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const { Schema, Types, model } = require('mongoose');
+
+const userSchema = new Schema({
+    username: { type: String, unique: true, required: true, trim: true },
+    email: { type: String, required: true, unique: true, match: [/.+\@.+\..+/, 'Must be a valid email address'] },
+    thoughts: [{ type: Types.ObjectId, ref: 'Thought' }],
+    friends: [{ type: Types.ObjectId, ref: 'User' }]
+}, {
+    toJSON: { virtuals: true },
+    id: false
+});
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+module.exports = User;
